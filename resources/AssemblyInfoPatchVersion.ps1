1dcf46f5a7fd8cf6d3a11583deb0c44098f28335
Alexey Melezhik <melezhik@gmail.com>
2018-10-02 20:39:43 +0000

    fix powershell issues

--- resources/AssemblyInfoPatchVersion.ps1
+++ resources/AssemblyInfoPatchVersion.ps1
@@ -1,14 +1,15 @@
+
 # from http://blogs.msdn.com/b/dotnetinterop/archive/2008/04/21/powershell-script-to-batch-update-assemblyinfo-cs-with-new-version.aspx
 #
 # AssemblyInfoPatchVersion.ps1
 #
 # Set the version in all the AssemblyInfo.cs or AssemblyInfo.vb files in any subdirectory.
 #
-# usage:  
-#  from cmd.exe: 
+# usage:
+#  from cmd.exe:
 #     powershell.exe AssemblyInfoPatchVersion.ps1  2.8.3.0
-# 
-#  from powershell.exe prompt: 
+#
+#  from powershell.exe prompt:
 #     .\AssemblyInfoPatchVersion.ps1  2.8.3.0
 #
 # last saved Time-stamp: <Wednesday, April 23, 2008  11:46:40  (by dinoch)>
@@ -30,15 +31,16 @@ function Usage
 function Update-SourceVersion
 {
 
-  Param (
-    [string]$Version,
-    [string]$Revision
-  )
 
-  $NewVersion = 'AssemblyVersion("' + $Version + '")';
-  $NewFileVersion = 'AssemblyFileVersion("' + $Version + '.' + $Revision + '")';
 
-  foreach ($o in $input) 
+  $NewVersion = "AssemblyVersion(" + $assembly_version  + '")';
+   
+  $NewFileVersion = 'AssemblyFileVersion("' + $assembly_version + '.' + $revision + '")';
+  
+  echo $NewVersion;
+  echo $NewFileVersion;
+  
+  foreach ($o in $input)
   {
     Write-output $o.FullName
     $TmpFile = $o.FullName + ".tmp"
@@ -47,38 +49,42 @@ function Update-SourceVersion
         %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $NewVersion } |
         %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', $NewFileVersion }  |
         Set-Content $TmpFile -encoding utf8
-    
+
     move-item $TmpFile $o.FullName -force
   }
 }
 
 
-function Update-AllAssemblyInfoFiles ( $version, $revision )
+function Update-AllAssemblyInfoFiles
 {
-  foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" ) 
+
+  #echo $version ;
+  #echo $revision ;
+  
+  foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" )
   {
-    get-childitem -recurse |? {$_.Name -eq $file} | Update-SourceVersion $version, $revision ;
+    get-childitem -recurse |? {$_.Name -eq $file} | Update-SourceVersion;
   }
 }
 
 
-# validate arguments 
+# validate arguments
 
 echo "AssemblyVersion: ";
 echo $args[0];
 
 echo "Revision: ";
-echo $env:BUILD_BUILDID;
+echo "1000"
 
 $assembly_version = $args[0];
-$revision = $env:BUILD_BUILDID
+$revision = '1000'
 
 
 $r= [System.Text.RegularExpressions.Regex]::Match("$assembly_version.$revision", "^[0-9]+(\.[0-9]+){1,3}$");
 
 if ($r.Success)
 {
-  Update-AllAssemblyInfoFiles($assembly_version, $revision);
+  Update-AllAssemblyInfoFiles
 }
 else
 {
@@ -88,3 +94,4 @@ else
   Usage ;
   exit 1;
 }
+
