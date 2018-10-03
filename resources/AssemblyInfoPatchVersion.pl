aab348a2674fe332b7a1fe76ba0c633daf42f4bd
Alexey Melezhik <melezhik@gmail.com>
2018-10-03 20:56:49 +0000

    ok

--- resources/AssemblyInfoPatchVersion.pl
+++ resources/AssemblyInfoPatchVersion.pl
@@ -1,17 +1,21 @@
 use File::Find;
 
-finddepth(\&wanted);
-
 my $r = shift or die "usage: AssemblyInfoPatchVersion.pl revision";
 
+find( { wanted => \&wanted ,  follow => 1 }, ".");
+
+
 sub wanted {
-  my $f = shift;
+
+  my $f = $_;
+
 
   # AssemblyFileVersion("2.5.0.0")
 
   if ($f=~/\.cs$/){
-    print "patch $File::Find::fullname ...\n";
-    my $cmd = 'perl -i -p -e "s{(AssemblyFileVersion.*\d+\.\d+\.\d+\.)(\d+)}[\$1\$r]g" '.$File::Find::fullname;
+    print "patch  ", $File::Find::fullname , " ...\n";
+    my $cmd = 'perl -i -p -e "BEGIN { \$r = qq{'.$r.'} }; s{(AssemblyFileVersion.*\d+\.\d+\.\d+\.)(\d+)}[\$1\$r]g" '.($File::Find::fullname);
+    print "run cmd: $cmd\n";
     system( $cmd ) == 0 or die "cannot patch $f, error executing cmd $cmd: $!";
   }
 }
