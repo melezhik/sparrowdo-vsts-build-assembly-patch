eda881ae4093c9bad55fd742d0e9a427fc20eedf
Alexey Melezhik <melezhik@gmail.com>
2018-10-03 21:17:16 +0000

    windows portable solution

--- resources/AssemblyInfoPatchVersion.pl
+++ resources/AssemblyInfoPatchVersion.pl
@@ -1,20 +1,22 @@
 use File::Find;
+use File::Spec;
 
-my $r = shift or die "usage: AssemblyInfoPatchVersion.pl revision";
+my $dir = shift or die "usage: AssemblyInfoPatchVersion.pl dir revision";
+my $r = shift or die "usage: AssemblyInfoPatchVersion.pl dir revision";
 
-find( { wanted => \&wanted ,  follow => 1 }, ".");
+find( { wanted => \&wanted ,  follow => 1 }, $dir );
 
 
 sub wanted {
 
-  my $f = $_;
+  my $f = File::Spec->rel2abs($_);
 
 
   # AssemblyFileVersion("2.5.0.0")
 
   if ($f=~/\.cs$/){
-    print "patch  ", $File::Find::fullname , " ...\n";
-    my $cmd = 'perl -i -p -e "BEGIN { \$r = qq{'.$r.'} }; s{(AssemblyFileVersion.*\d+\.\d+\.\d+\.)(\d+)}[\$1\$r]g" '.($File::Find::fullname);
+    print "patch  ", $f , " ...\n";
+    my $cmd = 'perl -i -p -e "BEGIN { \$r = qq{'.$r.'} }; s{(AssemblyFileVersion.*\d+\.\d+\.\d+\.)(\d+)}[\$1\$r]g" '.$f;
     print "run cmd: $cmd\n";
     system( $cmd ) == 0 or die "cannot patch $f, error executing cmd $cmd: $!";
   }
